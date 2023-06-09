81e6994c3c05cd8a7b4b090f14c0127746c48d9a
Askari <askarir64@gmail.com>
2023-06-10 01:09:45 +0530

    fixed bug. Timer runs only on initial render. Added comments

--- src/components/Hooks/useEffect/Timer.js
+++ src/components/Hooks/useEffect/Timer.js
@@ -2,17 +2,17 @@ import { useState , useEffect} from "react";
 
 function Timer() {
 
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0); // state which stores number of seconds and setter function
 
-     useEffect(() => {
-        const timer = setTimeout(() => {
+     useEffect(() => {    
+        const timer = setTimeout(() => {  // created timer var to store setTimeout() 
             setCount((count) => count+1);
         },1000);
         return() => {
-            clearTimeout(timer);
-        };
+            clearTimeout(timer);  // clearTimeout(timer) to unmount and to prevent memory leaks.
+        };   
     
-    },[]);
+    },[]); // empty array '[]' dependency runs the effect only on first render
 
     const HandleClick = (e) => {
         setCount(0);
