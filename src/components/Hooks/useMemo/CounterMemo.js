4fa4cf8d3f3491c7ef57209c6400e45a01147ccd
Askari <askarir64@gmail.com>
2023-07-04 18:31:42 +0530

    useMemo hook

--- src/components/Hooks/useMemo/CounterMemo.js
+++ src/components/Hooks/useMemo/CounterMemo.js
@@ -1,31 +1,36 @@
 import React, {useState} from 'react'
+import { useMemo } from 'react';
 
 function CounterMemo() {
 
     const [countOne, setCountOne] = useState(0);
     const [countTwo, setCountTwo] = useState(0);
 
-    const increaseOne = () => {
+     const increaseOne = () => {
         setCountOne(countOne => countOne + 1);
     }
 
-    const increaseTwo = () => {
+     const increaseTwo = () => {
         setCountTwo(countTwo => countTwo + 2);
     }
 
-    const isEven = () => {
+     const isEven = useMemo(() => {
         let i= 0;
         while (i< 999999999) { // to make calculation expensive
             i++
         };
         return countOne % 2 === 0;
-    }
+    },[countOne])
+
+    //useMemo cache result of function while useCallback cache the function itself
+    //useMemo(callback function , [dependency])
+     
     
 
   return (
     <>
     <button onClick={increaseOne}>Increase One - {countOne}</button>
-    <span>{isEven()?'Even':'Odd'}</span>
+    <span>{isEven?'Even':'Odd'}</span>
     <br/>
     <button onClick={increaseTwo}>Increase Two - {countTwo}</button>
     </>
